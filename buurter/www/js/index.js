7838e03ee326b8640a5fefdbff49aee7461b7702
Marijn Stuyfzand <majostu@hotmail.com>
2015-06-23 15:58:52 +0200

    Activiteit toevoegen

--- buurter/www/js/index.js
+++ buurter/www/js/index.js
@@ -245,31 +245,33 @@ module.controller('RegisterController', function($rootScope, $scope, $compile, $
 
 module.controller('HomeController', function($rootScope, $scope, $compile, $http, localStorageService, ngFB) { 
 
-
 	ons.ready(function() {
 		console.log($rootScope.data);
+		
+		$scope.fav_bar = {
+			name: "indebuurt"	
+		};
+		
 	});
-	
-	
-			//Need to watch on the session storage (FB AUTH KEY)
-		if(is.set(sessionStorage.fbAccessToken)){
-			ngFB.api({path: '/me'}).then(
-			function(user) {
-				console.log(JSON.stringify(user));
-				$scope.user = user;
-				loggedin = true;
-				console.log(loggedin);			
-			},
-			errorHandler);
-		}
-		function errorHandler(error) { 
-			$scope.data = {
-				show: false,
-				hide: true
-			};
-			//alert(error.message);
-		}
-
+		
+	//Need to watch on the session storage (FB AUTH KEY)
+	if(is.set(sessionStorage.fbAccessToken)){
+		ngFB.api({path: '/me'}).then(
+		function(user) {
+			console.log(JSON.stringify(user));
+			$scope.user = user;
+			loggedin = true;
+			console.log(loggedin);			
+		},
+		errorHandler);
+	}
+	function errorHandler(error) { 
+		$scope.data = {
+			show: false,
+			hide: true
+		};
+		//alert(error.message);
+	}
 	
 });
 
@@ -296,63 +298,416 @@ module.controller('ContactenController', function($scope) {
 	});
 });
 
-module.controller('NieuweActiviteitController', function($scope, transformRequestAsFormPost, $http) { 
+module.controller('NieuweActiviteitController', function($rootScope, $scope, $compile, $http, localStorageService, transformRequestAsFormPost) { 
 	ons.ready(function() {
+			
+		$scope.dialogs = {};
+   
+		$scope.AddActivityShow = function(dlg) {
+			if (!$scope.dialogs[dlg]) {
+		    	ons.createDialog(dlg).then(function(dialog) {
+		        	$scope.dialogs[dlg] = dialog;
+					dialog.show();
+		      	});
+		   	}
+		    else {
+		    	$scope.dialogs[dlg].show();
+		    }
+		}
+				
+		function geoAddActivity() {
+	    
+		    var options = { frequency: 5000, enableHighAccuracy: true};
+			watchID = navigator.geolocation.getCurrentPosition(onSuccess, onError, options);
+		    
+		    function onSuccess(position) {
+				var lat = position.coords.latitude;
+				var lng = position.coords.longitude;
+				
+				localStorage.setItem("latitude", lat);
+				localStorage.setItem("longitude", lng);
+								
+				$scope.map;
+		        $scope.markers = [];
+		        $scope.markerId = 1;
+
+	            var latlng = new google.maps.LatLng(lat, lng);
+	            var myOptions = {
+	                zoom: 16,
+	                center: latlng,
+	                mapTypeId: google.maps.MapTypeId.ROADMAP,
+	                disableDefaultUI: true
+	            };
+	            
+	           var map = new google.maps.Map(document.getElementById("map_add_activity"), myOptions); 
+	            
+	            var contentString = "<div><a ng-click='clickTest()'>Mijn locatie</a></div>";
+				var compiled = $compile(contentString)($scope);
 		
-	 $scope.list = [ { "id" : "1" , "name" : "Sport" } , { "id" : "2" , "name" : "Eten en drinken" } , { "id" : "3" , "name" : "Shoppen"} , { "id" : "4" , "name" : "Muziek"}, { "id" : "5" , "name" : "Film"}, { "id" : "6" , "name" : "Spel"}, { "id" : "7" , "name" : "Video Games"}, { "id" : "8" , "name" : "Fotografie"}, { "id" : "9" , "name" : "Relaxen"} , { "id" : "10" , "name" : "Reizen"}, { "id" : "11" , "Overige" : "Shoppen"}        ];
-    
-		$scope.submit = function (){
-			
-		$http({
-		   url:'http://broekhuizenautomaterialen.nl/directa/data.php',
-		   method:"POST",
-		   headers: {
-			'X-Requested-With': 'XMLHttpRequest',
-			'Content-Type': 'application/x-www-form-urlencoded'
-		   },
-		   transformRequest: transformRequestAsFormPost,
-			data    : eval({ 
-			'slug' : "activities", 
-			acitivity_id : ""+$scope.model_selected.id+"", 
-			imgsrc : ""+$scope.imgsrc+"", 
-			title : ""+$scope.title+"",
-			description : ""+$scope.description+"",
-			from_time: ""+$scope.inputDate+$scope.inputTime1+"",
-			to_time: ""+$scope.inputDate+$scope.inputTime2+"",
-			lat: ""+lat+"",
-			lon: ""+lon+""
-			}),  // pass in data as strings
-			
-			isArray: true,
-			callback: ''
-		}).success(function(data) {
-			
-			if (!data) {
-			  // if not successful, bind errors to error variables
-			  console.log('error');
-			} else {
-			  // if successful, bind success message to message
+				var infowindow = new google.maps.InfoWindow({
+					content: compiled[0]
+		    	});
+		    			    	
+		    	var icon_myPos = {
+				    url: "img/wheelchair.png",
+				    /*scaledSize: new google.maps.Size(50, 50), // scaled size
+				    origin: new google.maps.Point(0,0), // origin
+				    anchor: new google.maps.Point(0, 0) // anchor*/
+				};
+								
+				var marker = new google.maps.Marker({
+					position: latlng,
+					map: map,
+					title: 'Mijn locatie',
+					optimized: false,
+					animation: google.maps.Animation.DROP
+					//icon: icon_myPos
+		    	});
+		
+				google.maps.event.addListener(marker, 'click', function() {
+					infowindow.open(map,marker);
+		    	});
+		        
+	            $scope.overlay = new google.maps.OverlayView();
+	            $scope.overlay.draw = function() {}; // empty function required
+	            $scope.overlay.setMap(map);
+	            $scope.element = document.getElementById('map_add_activity');
+	            $scope.hammertime = Hammer($scope.element).on("doubletap", function(event) {
+	                $scope.addOnClick(event);
+	            });
+	            
+	            var oms = new OverlappingMarkerSpiderfier(map, {markersWontMove: true, markersWontHide: true, keepSpiderfied: true, nearbyDistance: 20, legWeight: 1});
+					        		
+		        //Add single Marker
+		        $scope.addOnClick = function(event) {
+		            var x = event.gesture.center.pageX;
+		            var y = event.gesture.center.pageY-44;
+		            var point = new google.maps.Point(x, y);
+		            var coordinates = $scope.overlay.getProjection().fromContainerPixelToLatLng(point);
+					
+					var iconBase = 'img/';
+		            var marker = new google.maps.Marker({
+		                position: coordinates,
+		                map: map,
+		                optimized: false,
+		                //icon: iconBase + 'markerunrated.png'
+		            });
+		           		            
+		            marker.id = $scope.markerId;
+		            $scope.markerId++;
+		            $scope.markers.push(marker);
+		            		        
+		            ons.notification.confirm({
+			            title: 'Bevestiging',
+	                    message: 'Weet je zeker dat je op deze locatie een activiteit wilt organiseren?',
+	                    callback: function(idx) {
+	                        switch(idx) {
+	                            case 0:
+	                                for (var i = 0; i < $scope.markers.length; i++) {
+	                                    if ($scope.markers[i].id == marker.id) {
+	                                        //Remove the marker from Map                  
+	                                        $scope.markers[i].setMap(null);
+	
+	                                        //Remove the marker from array.
+	                                        $scope.markers.splice(i, 1);
+	                                    }
+	                                }
+	                                ons.notification.alert({
+		                                messageHTML: '<div><ons-icon icon="fa-ban" style="color:#9d0d38; font-size: 28px;"></ons-icon></div>',
+										title: 'Activiteit verwijderd',
+										buttonLabel: 'OK'
+	                                });
+	                                break;
+	                            case 1:	 
+	                            								
+									$scope.AddActivityShow('add-activity-form.html');
+																								
+									localStorage.setItem("marker-latitude", $scope.markers[0].position.A);
+		                            localStorage.setItem("marker-longitude", $scope.markers[0].position.F);	
+		                                                                       	
+	                                break;
+	                        }
+	                    }
+	                });
+		        };  
+		        
+		        $scope.map = map;		       
+		         				
+			}
 						
-						ons.notification.alert({
-							messageHTML: '<div>Activititeit toegevoegd!</div>',
-							// or messageHTML: '<div>Message in HTML</div>',
-							title: data.title,
-							buttonLabel: 'OK',
-							cancelable: true,
-							animation: 'default', // or 'none'
-							// modifier: 'optional-modifier'
-							callback: function() {
-								menu.setMainPage('home.html', {closeMenu: true})
+			function onError(error) {
+				alert("message: " + error.message);
+				localStorage.setItem("message", error.message);
+			}
+		
+		}
+	    	
+	  	google.maps.event.addDomListener(window, 'load', geoAddActivity());
+				
+	});
+});
+
+module.controller('AddActivityFormController', function($rootScope, $scope, $compile, $http, localStorageService, transformRequestAsFormPost) {
+	ons.ready(function() {
+				
+		$scope.list = [ { "id" : "1" , "name" : "Sport" } , { "id" : "2" , "name" : "Eten en drinken" } , { "id" : "3" , "name" : "Shoppen"} , { "id" : "4" , "name" : "Muziek"}, { "id" : "5" , "name" : "Film"}, { "id" : "6" , "name" : "Spel"}, { "id" : "7" , "name" : "Video Games"}, { "id" : "8" , "name" : "Fotografie"}, { "id" : "9" , "name" : "Relaxen"} , { "id" : "10" , "name" : "Reizen"}, { "id" : "11" , "name" : "Overige"} ];
+		
+  		var marker_latitude = localStorage.getItem("marker-latitude");
+		var marker_longitude = localStorage.getItem("marker-longitude");	
+   		
+		$scope.AddActivitySubmit = function() {
+					
+        	var WC_latitude = marker_latitude;
+        	var WC_longitude = marker_longitude;
+       	
+        	var json = (function () {
+				var json = null;
+				$.ajax({
+					'type':'GET',
+					'async': false,
+					'global': false,
+					'url': "http://maps.googleapis.com/maps/api/geocode/json?latlng=" + WC_latitude + "," + WC_longitude + "&sensor=true",
+					'dataType': "json",
+					'success': function (data) {
+						json = data;
+					}
+				});
+				return json;										
+			})();
+			
+			WC_latitude = json.results[0].geometry.location.lat;									
+			WC_longitude = json.results[0].geometry.location.lng;
+			var WC_address = json.results[0].formatted_address;
+	                            									   		
+			$http({
+			   url:'http://broekhuizenautomaterialen.nl/directa/data.php',
+			   method:"POST",
+			   headers: {
+				'X-Requested-With': 'XMLHttpRequest',
+				'Content-Type': 'application/x-www-form-urlencoded'
+			   },
+			   transformRequest: transformRequestAsFormPost,
+				data : eval({ 
+				'slug' : "activities", 
+				imgsrc : ""+$scope.imgsrc+"", 
+				title : ""+$scope.activity_title+"",
+				category : ""+$scope.model_selected.name+"",
+				date : ""+$scope.activity_date+"",
+				date : ""+$scope.activity_date+"",
+				from_time : ""+$scope.activity_time_1+"",
+				to_time : ""+$scope.activity_time_2+"",
+				address : ""+WC_address+"",
+				latitude : ""+WC_latitude+"",
+				longitude : ""+WC_longitude+""
+				}),  // pass in data as strings
+				
+				isArray: true,
+				callback: ''
+			}).success(function(data) {				
+				if (!data) {
+					// if not successful, bind errors to error variables
+					console.log(data);
+					console.log('error');
+					
+					ons.notification.alert({
+                        messageHTML: '<div><ons-icon icon="fa-ban" style="color:#9d0d38; font-size: 28px;"></ons-icon></div>',
+						title: 'Oeps... er is iets fout gegaan',
+						buttonLabel: 'OK',
+						callback: function() {
+															
+						}
+                    });	
+				} else {
+				  	// if successful, bind success message to message
+				  	console.log(data);
+				  	console.log('success');
+				  
+				  	ons.notification.alert({
+                    	messageHTML: '<div><ons-icon icon="fa-check" style="color:#61b76b; font-size: 28px;"></ons-icon></div>',
+						title: 'De activiteit is succesvol toegevoegd',
+						buttonLabel: 'OK',
+						callback: function() {
+							introNavigator.popPage('home.html');										
+						}
+                	});			                              	                                	
+				}	
+			 });
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			/*$http({
+				url:'http://marijnstuyfzand.nl/mia6/handyfriendly/www/php/insert_marker.php',
+				method:"POST",
+				headers: {
+					'X-Requested-With': 'XMLHttpRequest',
+					'Content-Type': 'application/x-www-form-urlencoded'
+				},
+			    data: $.param({ 
+					slug: "toilets",
+					device_id: $scope.data[0],  
+					name: $scope.data[1],
+					address: $scope.data[2],
+					latitude: $scope.data[3], 
+					longitude: $scope.data[4],
+					rating: $scope.data[5],
+				}),  // pass in data as strings
+				isArray: true,
+				callback: ''
+		  	}).success(function(data) {
+				if (!data) {
+					// if not successful, bind errors to error variables
+					console.log(data);
+					console.log('error');
+					
+					ons.notification.alert({
+                        messageHTML: '<div><ons-icon icon="fa-ban" style="color:#9d0d38; font-size: 28px;"></ons-icon></div>',
+						title: 'Oeps... er is iets fout gegaan',
+						buttonLabel: 'OK',
+						callback: function() {
+							
+							localStorage.removeItem("profile_id");
+							location.reload();
+						
+						}
+                    });
+					
+				} else {
+				  	// if successful, bind success message to message
+				  	console.log(data);
+				  	console.log('success');
+				  	
+				  	var json = (function () {
+						var json = null;
+						$.ajax({
+							'type':'GET',
+							'async': false,
+							'global': false,
+							'url': "http://marijnstuyfzand.nl/mia6/handyfriendly/www/php/get_marker.php",
+							'dataType': "json",
+							'data': $.param({ 
+								get_toilets: "all"
+							}),
+							'success': function (data) {
+								json = data;
 							}
 						});
-			}
+						return json;
+					})();
+					
+					var profile_data_id = $(json).last()[0].id;
+					//var added_id = $(json).last()[0].id;
+									  	
+				  	$scope.data = [];
 			
-		  });
-			
-		}
+				  	$scope.data.push(profile_data_id );
+					$scope.data.push(Device_id);			
+					$scope.data.push($scope.rating2);
+					$scope.data.push($scope.marker_comment);
+					$scope.data.push(Device_name);
+					
+					//$scope.data.push(added_id);
+											
+					console.log($scope.data);
+					
+					$http({
+						url:'http://marijnstuyfzand.nl/mia6/handyfriendly/www/php/insert_marker.php',
+						method:"POST",
+						headers: {
+							'X-Requested-With': 'XMLHttpRequest',
+							'Content-Type': 'application/x-www-form-urlencoded'
+						},
+					    data: $.param({ 
+							slug: "saveRateComment",
+							toilet_id: $scope.data[0], 
+							device_id: $scope.data[1],  
+							rating: $scope.data[2],
+							comment: $scope.data[3],
+							device_name: $scope.data[4],
+							//added_id: $scope.data[5]
+						}),  // pass in data as strings
+						isArray: true,
+						callback: ''
+				  	}).success(function(data) {
+						if (!data) {
+							// if not successful, bind errors to error variables
+							console.log(data);
+							console.log('error');
+							
+							ons.notification.alert({
+		                        messageHTML: '<div><ons-icon icon="fa-ban" style="color:#9d0d38; font-size: 28px;"></ons-icon></div>',
+								title: 'Oeps... er is iets fout gegaan',
+								buttonLabel: 'OK',
+								callback: function() {
+									
+									localStorage.removeItem("profile_id");
+									location.reload();
+								
+								}
+		                    });
+							
+						} else {
+						  	// if successful, bind success message to message
+						  	console.log(data);
+						  	console.log('success');
+						  
+						  	ons.notification.alert({
+		                    	messageHTML: '<div><ons-icon icon="fa-check" style="color:#25c2aa; font-size: 28px;"></ons-icon></div>',
+								title: 'WC toegevoegd',
+								buttonLabel: 'OK',
+								callback: function() {
+															
+									localStorage.removeItem("profile_id");
+									location.reload();
+																			
+								}
+		                	});			                              	                                	
+						}
+					});
+							                              	                                	
+				}
+			});*/
+						  				  								  				
+		};
 		
 	});
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 module.controller('InviteController', function($scope) { 
 	ons.ready(function() {
